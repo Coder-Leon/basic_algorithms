3a3ef08c778384b486f9237af1a5a63d680764ef
Coder-Leon <332785940@qq.com>
2018-08-09 15:53:50 +0800

    add SelectionSort

--- src/Code_02_SelectionSort.java
+++ src/Code_02_SelectionSort.java
@@ -1,36 +1,35 @@
 import java.util.Arrays;
 
-public class Code_01_BubbleSort {
-    public static void bubbleSort(int[] arr) {
+public class Code_02_SelectionSort {
+    public static void selectionSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
-        for (int i = arr.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    swap(arr, j, j + 1);
-                }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
             }
+            swap(arr, i, minIndex);
         }
     }
 
-    // i == j 时，不可用，否则交换结果为 0
     public static void swap(int[] arr, int i, int j) {
-        arr[i] = arr[i] ^ arr[j];
-        arr[j] = arr[i] ^ arr[j];
-        arr[i] = arr[i] ^ arr[j];
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 
     public static void main(String[] args) {
         int testTime = 500000;
-        int maxSize = 50;
+        int maxSize = 100;
         int maxValue = 100;
         boolean succeed = true;
         // test case
         for (int i = 0; i < testTime; i++) {
             int[] arr1 = generateRandomArray(maxSize, maxValue);
             int[] arr2 = copyArray(arr1);
-            bubbleSort(arr1);
+            selectionSort(arr1);
             comparator(arr2);
             if (!isEqual(arr1, arr2)) {
                 succeed = false;
@@ -41,19 +40,19 @@ public class Code_01_BubbleSort {
         }
         System.out.println(succeed ? "Bingo!!!" : "Shit!!!");
 
-        int arr[] = generateRandomArray(maxSize, maxValue);
+        int[] arr = generateRandomArray(maxSize, maxValue);
         printArray(arr);
-        bubbleSort(arr);
+        selectionSort(arr);
         printArray(arr);
     }
 
     // for test
-    private static void comparator(int[] arr) {
+    public static void comparator(int[] arr) {
         Arrays.sort(arr);
     }
 
     // for test
-    private static int[] generateRandomArray(int maxSize, int maxValue) {
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
         int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
@@ -102,5 +101,4 @@ public class Code_01_BubbleSort {
         }
         System.out.println();
     }
-
 }
