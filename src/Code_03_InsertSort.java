f84f338ea5e3381d32fd8212f1b197951337ee09
Coder-Leon <332785940@qq.com>
2018-08-09 22:49:11 +0800

    add InsertSort

---
+++ src/Code_03_InsertSort.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+public class Code_03_InsertSort {
+    public static void insertSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                swap(arr, j, j+1);
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 50;
+        int maxValue = 100;
+        boolean succeed = true;
+        // test case
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = Arrays.copyOf(arr1, arr1.length);
+            insertSort(arr1);
+            comparator(arr2);
+            if (!Arrays.equals(arr1, arr2)) {
+                succeed = false;
+                System.out.println(Arrays.toString(arr1));
+                System.out.println(Arrays.toString(arr2));
+                break;
+            }
+        }
+        System.out.println(succeed ? "Bingo!!!" : "Shit!!!");
+
+        int arr[] = generateRandomArray(maxSize, maxValue);
+        System.out.println(Arrays.toString(arr));
+        insertSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // for test
+    private static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    private static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+}
