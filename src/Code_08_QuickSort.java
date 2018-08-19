81657f8084a4630621e718ddbcd6fb22e1dcfafb
Coder-Leon <332785940@qq.com>
2018-08-19 21:33:21 +0800

    add QuickSort

---
+++ src/Code_08_QuickSort.java
@@ -0,0 +1,82 @@
+import java.util.Arrays;
+
+public class Code_08_QuickSort {
+    public static void quickSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            swap(arr, left + (int) (Math.random() * (right - left + 1)), right);
+            int[] p = partition(arr, left, right);
+            quickSort(arr, left, p[0] - 1);
+            quickSort(arr, p[1] + 1, right);
+        }
+    }
+
+    public static int[] partition(int[] arr, int left, int right) {
+        int less = left - 1;
+        int more = right;
+        while (left < more) {
+            if (arr[left] < arr[right]) {
+                swap(arr, ++less, left++);
+            } else if (arr[left] > arr[right]) {
+                swap(arr, --more, left);
+            } else {
+                left++;
+            }
+        }
+        swap(arr, more, right);
+        return new int[]{less + 1, more};
+    }
+
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 50;
+        int maxValue = 100;
+        boolean succeed = true;
+        // test case
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = Arrays.copyOf(arr1, arr1.length);
+            quickSort(arr1);
+            comparator(arr2);
+            if (!Arrays.equals(arr1, arr2)) {
+                succeed = false;
+                System.out.println(Arrays.toString(arr1));
+                System.out.println(Arrays.toString(arr2));
+                break;
+            }
+        }
+        System.out.println(succeed ? "Bingo!!!" : "Shit!!!");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        System.out.println(Arrays.toString(arr));
+        quickSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // for test
+    private static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    private static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+}
