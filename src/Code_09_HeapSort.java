dab2d559e6b0b6f8c46e16504213d7ce62f764c6
Coder-Leon <332785940@qq.com>
2018-08-20 21:05:27 +0800

    add HeapSort

---
+++ src/Code_09_HeapSort.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+
+public class Code_09_HeapSort {
+    public static void heapSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            heapInsert(arr, i);
+        }
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+        while (heapSize > 0) {
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
+    }
+
+    public static void heapify(int[] arr, int index, int heapSize) {
+        int left = index * 2 + 1;
+        while (left < heapSize) {
+            int largestIndex = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+            largestIndex = arr[largestIndex] > arr[index] ? largestIndex : index;
+            if (largestIndex == index) {
+                break;
+            }
+            swap(arr, largestIndex, index);
+            index = largestIndex;
+            left = index * 2 + 1;
+        }
+    }
+
+    public static void heapInsert(int[] arr, int index) {
+        while (arr[index] > arr[(index - 1) / 2]) {
+            swap(arr, index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 50;
+        int maxValue = 100;
+        boolean succeed = true;
+        // test case
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = Arrays.copyOf(arr1, arr1.length);
+            heapSort(arr1);
+            comparator(arr2);
+            if (!Arrays.equals(arr1, arr2)) {
+                succeed = false;
+                System.out.println(Arrays.toString(arr1));
+                System.out.println(Arrays.toString(arr2));
+                break;
+            }
+        }
+        System.out.println(succeed ? "Bingo!!!" : "Shit!!!");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        System.out.println(Arrays.toString(arr));
+        heapSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // for test
+    private static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    private static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+}
